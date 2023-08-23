85489c555ce55c0f4f4120cb7dee52bdf2bdca15
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2023-08-24 01:35:10 +0530

    Create Create_file.sh

---
+++ Create_file.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+Create a file in a specifi path and add some conent into that file.
+echo "Your cuurent directory is "
+pwd
+read -p "Enter you desired location here" location
+echo "Now your current location is "
+cd $location
+pwd
+read -p "Enter a file name: " filename
+read -p "Enter extenison you want for the file: " exe
+read -p "How Many File You want to create?? " end_no
+
+seq 1 $end_no | xargs -I {} touch $filename{}.$exe
+ls
